db1ea30a0bf8b2c67b9c7aa0e32fecf7f5db01a2
Martin Beaufils <martin.beaufils@polytechnique.edu>
2025-03-04 10:26:53 +0100

    Restore changes

--- train.py
+++ train.py
@@ -43,7 +43,10 @@ elif data_mode == 'Mall':
     from datasets.Mall.setting import cfg_data
 elif data_mode == 'UCSD':
     from datasets.UCSD.loading_data import loading_data
-    from datasets.UCSD.setting import cfg_data 
+    from datasets.UCSD.setting import cfg_data
+elif data_mode == 'DroneCrowd':
+    from datasets.DroneCrowd.loading_data import loading_data
+    from datasets.DroneCrowd.setting import cfg_data
 
 
 #------------Prepare Trainer------------
